90f97dce5112bff526c0fb62358b757ca1b245f6
20r01a67b8 <95232768+praneethkumar15@users.noreply.github.com>
2023-06-14 15:46:22 +0530

    Create elementtree.py

---
+++ elementtree.py
@@ -0,0 +1,96 @@
+# encoding: utf-8
+# module _elementtree
+# from (pre-generated)
+# by generator 1.147
+# no doc
+# no imports
+# functions
+def SubElement(*args, **kwargs): # real signature unknown
+ pass
+# classes
+class Element(object):
+ # no doc
+ def append(self, *args, **kwargs): # real signature unknown
+ pass
+ def clear(self, *args, **kwargs): # real signature unknown
+ pass
+ def extend(self, *args, **kwargs): # real signature unknown
+ pass
+ def find(self, *args, **kwargs): # real signature unknown
+ pass
+ def findall(self, *args, **kwargs): # real signature unknown
+ pass
+ def findtext(self, *args, **kwargs): # real signature unknown
+ pass
+ def get(self, *args, **kwargs): # real signature unknown
+ pass
+ def itertext(self, *args, **kwargs): # real signature unknown
+ pass
+ pass
+ def __delitem__(self, *args, **kwargs): # real signature unknown
+ """ Delete self[key]. """
+ pass
+ def __getattribute__(self, *args, **kwargs): # real signature unknown
+ """ Return getattr(self, name). """
+ pass
+ @staticmethod # known case of __new__
+ def __new__(*args, **kwargs): # real signature unknown
+ """ Create and return a new object. See help(type) for accurate signature. """
+ pass
+ def __repr__(self, *args, **kwargs): # real signature unknown
+ """ Return repr(self). """
+ pass
+ def __setitem__(self, *args, **kwargs): # real signature unknown
+ """ Set self[key] to value. """
+ pass
+ def __setstate__(self, *args, **kwargs): # real signature unknown
+ pass
+ def __sizeof__(self, *args, **kwargs): # real signature unknown
+ pass
+ attrib = property(lambda self: object(), lambda self, v: None, lambda self: None) # default
+ """A dictionary containing the element's attributes"""
+ tag = property(lambda self: object(), lambda self, v: None, lambda self: None) # default
+ """A string identifying what kind of data this element represents"""
+class ParseError(SyntaxError):
+ # no doc
+ def __init__(self, *args, **kwargs): # real signature unknown
+ pass
+ __weakref__ = property(lambda self: object(), lambda self, v: None, lambda self: None) # 
+default
+ """list of weak references to the object (if defined)"""
+class TreeBuilder(object):
+ # no doc
+ def close(self, *args, **kwargs): # real signature unknown
+ pass
+ def data(self, *args, **kwargs): # real signature unknown
+ pass
+ def end(self, *args, **kwargs): # real signature unknown
+ pass
+ def start(self, *args, **kwargs): # real signature unknown
+ pass
+ def __init__(self, *args, **kwargs): # real signature unknown
+ pass
+ @staticmethod # known case of __new__
+ def __new__(*args, **kwargs): # real signature unknown
+ """ Create and return a new object. See help(type) for accurate signature. """
+ pass
+class XMLParser(object):
+ # no doc
+ def close(self, *args, **kwargs): # real signature unknown
+ pass
+ def doctype(self, *args, **kwargs): # real signature unknown
+ pass
+ def feed(self, *args, **kwargs): # real signature unknown
+ pass
+ def _parse_whole(self, *args, **kwargs): # real signature unknown
+ pass
+ def _setevents(self, *args, **kwargs): # real signature unknown
+ pass
+ def __getattribute__(self, *args, **kwargs): # real signature unknown
+ """ Return getattr(self, name). """
+ pass
+ def __init__(self, *args, **kwargs): # real signature unknown
+ pass
+ @staticmethod # known case of __new__
+ def __new__(*args, **kwargs): # real signature unknown
+ pass
